1f807d39daf09d8c3f9918379b4fef6304b0958b
Toño de la Torre <antonio@codesai.com>
2022-09-13 16:26:35 +0200

    Refactor

--- katas-java/gilded-rose/src/main/java/com/gildedrose/GildedRose.java
+++ katas-java/gilded-rose/src/main/java/com/gildedrose/GildedRose.java
@@ -37,27 +37,19 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (isBackstage(item)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
             }
             item.sellIn = item.sellIn - 1;
+            if (item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
         }
         if (isBackstage(item)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (isBackstage(item)) {
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
@@ -69,41 +61,10 @@ class GildedRose {
                             item.quality = item.quality + 1;
                         }
                     }
-                }
             }
             item.sellIn = item.sellIn - 1;
-        }
-
-
-        if (!isSulfuras(item)) {
-//            item.sellIn = item.sellIn - 1;
-        }
-
-
-        if (!isAgedBrie(item)) {
-            if (!isBackstage(item)) {
-                if (!isSulfuras(item)) {
-
-
-                }
-            }
-
-        }
-
-
-        if (isBackstage(item)) {
-
             if (item.sellIn < 0) {
                 item.quality = item.quality - item.quality;
-            }
-        }
-
-        if (isAgedBrie(item)) {
-
-            if (item.sellIn < 0) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
             }
         }
 
